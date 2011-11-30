8100a47a53b767ed36d264fed0834c4a692a9426
Srijak Rijal <srijak@gmail.com>
2011-11-29 20:37:28 -0500

    fleshed out blobstored + tests. fragile.

--- blobstore.go
+++ blobstore.go
@@ -3,8 +3,6 @@ package blobstore
 import (
 	"os"
 	l4g "log4go.googlecode.com/hg"
-	"rand"
-	//	"fmt"
 )
 
 type IBlobStore interface {
@@ -49,19 +47,27 @@ func (b *BlobStore) Get(key *string, blob *[]byte) os.Error {
 		return err
 	}
 	copyLocally := false
+	var local_vnode_idx int
 	for i := range replicas {
 		// look for a local one first.
+
 		if isLocalVnode(replicas[i]) {
+			// this code assumes that there is only one matching local vnode.
+			// i.e that replicas aren't on the same server.
+			l4g.Debug("Key should be on local vnode: %s", replicas[i])
 			*blob, err = b.ls.Get(*key, replicas[i])
 			if err != nil {
 				// err is assumed to be coz data was missing.
 				// so, copy it locally once we get it.
+				l4g.Debug("Key not found on local vnode. Will read repair vode %s for key: %s", replicas[i], *key)
 				copyLocally = true
+				local_vnode_idx = i
 			} else {
 				// if no errors getting data locally,
 				// we are done
 				//TODO: verify data: 
 				//if hash doesn't match, get it from another replica.
+				l4g.Debug("Found data for key %s locally.", *key)
 				return nil
 			}
 		}
@@ -69,17 +75,30 @@ func (b *BlobStore) Get(key *string, blob *[]byte) os.Error {
 	// the fact that we are here means data wasn't found locally.
 	// if copyLocally is true, it means that we should store
 	// the data locally coz it should have been here.
-
-	idx := rand.Intn(len(replicas))
-	err = b.getRemoteBlob(key, blob, replicas[idx])
-	if err != nil {
-		//TODO: could try others.
-		return err
-	}
+	var local_vnode IVnode
 	if copyLocally {
-		go b.ls.Put(blob, *key, replicas[idx])
+		//remove local vnode from list so it isn't tried again.
+		local_vnode = replicas[local_vnode_idx]
+		replicas = append(replicas[:local_vnode_idx], replicas[local_vnode_idx+1:]...)
 	}
-	return nil
+	l4g.Debug("Data for key %s not found locally. Trying other replicas.", *key)
+	for idx := range replicas {
+		err = b.getRemoteBlob(key, blob, replicas[idx])
+		if err == nil {
+			l4g.Debug("Got data for key %s from remote: %s", *key, replicas[idx].String())
+			if copyLocally {
+
+				l4g.Debug("ReadRepair: Copying key %s to local vnode: %s", *key, local_vnode.String())
+
+				go b.ls.Put(blob, *key, local_vnode)
+			}
+			return nil
+		} else {
+			l4g.Info("Couldnt get key %s from remote replica %s. Error: %s", *key, replicas[idx], err.String())
+		}
+	}
+
+	return os.NewError("Could not get from any of the replicas")
 }
 
 func (b *BlobStore) getRemoteBlob(key *string, blob *[]byte, vn IVnode) os.Error {
@@ -125,29 +144,41 @@ func (b *BlobStore) Put(blob *[]byte, key *string) (err os.Error) {
 	for i := range replicas {
 		// look for a local one first.
 		if isLocalVnode(replicas[i]) {
+			l4g.Debug("Data should be on local vnode: %s", replicas[i])
 			err := b.ls.Put(blob, *key, replicas[i])
 			if err != nil {
 				// local put failed for some reason.
 				// TODO: how to handle?
 				return err
 			} else {
-				// if no errors getting data locally,
-				// we are done
-				//TODO: verify data: 
-				//if hash doesn't match, get it from another replica.
-
+				// if no errors putting data locally,
 				return nil
 			}
 		}
 	}
-	// the fact that we are here means it's not a local put.
-	// choose a random replica
-	idx := rand.Intn(len(replicas))
-	err = b.putRemoteBlob(blob, key, replicas[idx])
-	if err != nil {
-		//TODO: could try others.
-		return err
+	// the fact that we are here means it's not a local put or that the local
+	// put failed.
+	// Try to store on any replica. Note that we retry local here.
+	// also, we require one synchronous successfull write.
+	// the number of writes that need to succeed will be customizable.
+	l4g.Debug("Data should be on remote vnode.")
+	one_passed := false
+	for i := range replicas {
+
+		l4g.Debug("Trying to store to %s.", replicas[i].String())
+		if !one_passed {
+			err = b.putRemoteBlob(blob, key, replicas[i])
+			if err == nil {
+				l4g.Debug("Data stored on vnode %s.", replicas[i].String())
+				one_passed = true
+			}
+		} else {
+			go b.putRemoteBlob(blob, key, replicas[i])
+		}
 	}
 
-	return err
+	if one_passed {
+		return nil
+	}
+	return os.NewError("Could not put to any responsible nodes. TODO: hinted handoff")
 }
--- blobstore_test.go
+++ blobstore_test.go
@@ -3,6 +3,7 @@ package blobstore
 import (
 	. "launchpad.net/gocheck"
 	"os"
+	"time"
 	"fmt"
 )
 
@@ -10,14 +11,52 @@ func (s *Unit) TestBlobGet_isLocal_FoundLocally(c *C) {
 	bs := getTestBlobStore()
 
 	blob := []byte("srijak") // hash: 929845509
-	var key string
+	key := getHash(&blob)
 
-	err := bs.Put(&blob, &key)
+	// put data into local vnode using ILocalStore
+	hostname, _ := os.Hostname()
+	err := bs.ls.Put(&blob, key, &Vnode{offset: 9e8, hostname: hostname})
 	c.Check(err, IsNil)
+
+	// use blobstore to get the data.
 	got := make([]byte, 0)
 	err = bs.Get(&key, &got)
 	c.Check(err, IsNil)
 	c.Assert(string(got), Equals, string(blob))
+}
+
+func (s *Unit) TestBlobGet_isLocal_NotFoundLocally(c *C) {
+	// if a blob should be found locally, but isn't then:
+	//   get the blob from the remote server.
+	//	 return the blob AND
+	//   copy locally so next get will work locally.
+	bs := getTestBlobStore()
+
+	blob := []byte("srijak") // hash: 929845509
+	key := getHash(&blob)
+
+	// put blob on remote replica node
+	rc, err := bs.rsf.GetClient("notus1", bs.port)
+	err = rc.Put(&blob, &key)
+	c.Check(err, IsNil)
+
+	// verify blob not on local
+	hostname, _ := os.Hostname()
+	_, err = bs.ls.Get(key, &Vnode{offset: 9e8, hostname: hostname})
+	c.Check(err, Not(IsNil))
+
+	// get. This will get from remote and copy into local.
+	got := make([]byte, 0)
+	err = bs.Get(&key, &got)
+	c.Check(err, IsNil)
+	c.Assert(string(got), Equals, string(blob))
+
+	// wait a seconf to let the goroutine complete.
+	time.Sleep(1e9)
+	got_local, err := bs.ls.Get(key, &Vnode{offset: 9e8, hostname: hostname})
+	c.Check(err, IsNil)
+	c.Assert(string(got_local), Equals, string(blob))
+
 }
 
 func (s *Unit) TestBlobGet_isRemote_Forwarded(c *C) {
@@ -26,12 +65,60 @@ func (s *Unit) TestBlobGet_isRemote_Forwarded(c *C) {
 	blob := []byte("notsrijak") // hash: 2030091525
 	var key string
 
-	bs.Put(&blob, &key)
+	// put blob on remote replica node
+	rc, err := bs.rsf.GetClient("notus2", bs.port)
+	err = rc.Put(&blob, &key)
+	c.Check(err, IsNil)
 
+	// verify that we can get the blob when we hit blobstore
+	// ie the call is forwarded tot he correct remote store.
 	got := make([]byte, 0)
 	bs.Get(&key, &got)
-
 	c.Assert(string(got), Equals, string(blob))
+}
+
+func (s *Unit) TestBlobPut_isLocal_StoredLocally(c *C) {
+	bs := getTestBlobStore()
+
+	blob := []byte("srijak") // hash: 929845509
+	var key string
+
+	// put the data
+	err := bs.Put(&blob, &key)
+	c.Check(err, IsNil)
+
+	// verify the data is in localstore
+	hostname, _ := os.Hostname()
+	got, err := bs.ls.Get(key, &Vnode{offset: 9e8, hostname: hostname})
+
+	c.Check(err, IsNil)
+	c.Assert(string(got), Equals, string(blob))
+}
+
+func (s *Unit) TestBlobPut_isRemote_StoredRemotely(c *C) {
+	bs := getTestBlobStore()
+
+	blob := []byte("notsrijak") // hash:  2030091525
+	var key string
+
+	// put the data
+	err := bs.Put(&blob, &key)
+	c.Check(err, IsNil)
+
+	// verify the data is in relevant IRemotStore
+	vnodes, err := bs.ks.GetVnodes()
+	replicas, err := bs.rs.Replicas(key, vnodes)
+	c.Check(err, IsNil)
+
+	// wait a sec for go routines to put data on all replicas
+	time.Sleep(1e9)
+	for i := range replicas {
+		var got []byte
+		rc, err := bs.rsf.GetClient(replicas[i].GetHostname(), bs.port)
+		err = rc.Get(&key, &got)
+		c.Check(err, IsNil)
+		c.Assert(string(got), Equals, string(blob))
+	}
 }
 
 func getTestBlobStore() *BlobStore {
@@ -45,7 +132,7 @@ func getTestBlobStore() *BlobStore {
 	}
 
 	ks := &TestKeySpace{vnodes: vnodes}
-	rs := &SimpleRep{N: 1}
+	rs := &SimpleRep{N: 2}
 	ls := &TestLocalStore{make(map[string]string)}
 	rsf := &TestRemoteStoreFactory{make(map[string]IRemoteStore)}
 
@@ -79,6 +166,7 @@ port int) (IRemoteStore, os.Error) {
 	if val, ok := r.m[name]; ok {
 		return val, nil
 	}
+
 	trs := &TestRemoteStore{make(map[string]string)}
 
 	r.m[name] = trs
